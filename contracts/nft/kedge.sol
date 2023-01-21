c1532f41c18171c19fbdeb5adfea0f9626526ad1
0xApeiron <0xapeiron@proton.me>
2023-01-21 18:12:31 +0100

    added burn from owner

--- contracts/nft/kedge.sol
+++ contracts/nft/kedge.sol
@@ -46,27 +46,27 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Kedge is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+    uint256 private tokenID;
 
     constructor() ERC721("Kedge", "KDG") {}
-
-    function safeMint(address to, string memory uri) external onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
 
     function _burn(uint256 tokenId)
         internal
         override(ERC721, ERC721URIStorage)
     {
         super._burn(tokenId);
+    }
+
+    function mint(address to, string memory uri) external onlyOwner {
+        tokenID++;
+        _safeMint(to, tokenID);
+        _setTokenURI(tokenID, uri);
+    }
+
+    function burn(uint256 tokenId) external onlyOwner {
+        _burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
